a6b11cb9299bef0a28a99e3a979189aadad4376a
Michael Panagos <46898253+Sekers@users.noreply.github.com>
2022-09-12 09:49:56 -0500

    Update New-SecurePassword.ps1

--- Password Tools/New-SecurePassword.ps1
+++ Password Tools/New-SecurePassword.ps1
@@ -6,7 +6,7 @@ Write-Host "This script accepts credentials and then returns the password as an 
 Write-Host "Unlike a secure string, an encrypted standard string can be saved in a file for later use."
 Write-Host "The encrypted standard string can be converted back to its secure string format by using the ConvertTo-SecureString cmdlet."
 Write-Host "Note: The password can only be decrypted by the same account on the same computer it was encrypted from."
-Write-Host 'Use the "-VerifyADAccount" switch to first check the submitted credentials against your Active Directory domain for verification (requires the Microsoft ActiveDirectory PowerShell Module).' 
+Write-Host 'Optionally use the "-VerifyADAccount" switch to first check the submitted credentials against your Active Directory domain for verification (requires the Microsoft ActiveDirectory PowerShell Module).' 
 Write-Host "`n"
 function New-SecurePassword
 {
