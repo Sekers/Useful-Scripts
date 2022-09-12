b172027bc32329a9dfa5fee03bf2e0cebf016119
Michael Panagos <46898253+Sekers@users.noreply.github.com>
2022-09-12 10:36:23 -0500

    Update PowerShell Automation Example Script.ps1

--- Automation/PowerShell Automation Script Example/PowerShell Automation Example Script.ps1
+++ Automation/PowerShell Automation Script Example/PowerShell Automation Example Script.ps1
@@ -1,4 +1,6 @@
 # PowerShell Automation Script Example
+# A sample PowerShell script that can be used as a template for scheduled tasks or other automation and includes warning/error email support,
+# normal and debug logging, as well an example on how to use external configuration files.
 
 ############
 # OVERVIEW #
